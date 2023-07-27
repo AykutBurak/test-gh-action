81478a7df8ba1ff8ac4e288b77f675bc919a6f80
AykutBurak <a.buraksafak@gmail.com>
2023-07-27 13:22:45 +0300

    fix: path for validate

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const exec = require('@actions/exec');
 
 async function run() {
   try {
-    await exec.exec('python3', ['../../validate/validate_json.py']);
-    await exec.exec('python3', ['../../validate/validate_csv.py']);
+    await exec.exec('python3', ['../validate/validate_json.py']);
+    await exec.exec('python3', ['../validate/validate_csv.py']);
   } catch (error) {
     core.setFailed(error.message);
   }
