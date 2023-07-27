31f4bcf06a7716b2318e6d120342e3846daa078e
AykutBurak <a.buraksafak@gmail.com>
2023-07-27 14:13:44 +0300

    feat: add csv validation

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const core = require('@actions/core');
 const validateJSONs = require('./validateJsons')
+const validateCSVs = require('./validateCSVs')
 
 async function run() {
   try {
     validateJSONs()
+    await validateCSVs()
   } catch (error) {
     core.setFailed(error.message);
   }
--- validateCSVs.js
+++ validateCSVs.js
@@ -2,14 +2,14 @@ const fs = require('fs');
 const path = require('path')
 const CSVValidator = require('csv-file-validator')
 
-module.exports = function validateJSONs() {
+module.exports = function validateCSVs() {
   const CSVsInDir = fs.readdirSync('./lang').filter(file => path.extname(file) === '.csv');
 
-  CSVsInDir.forEach(file => {
+  return Promise.all(CSVsInDir.map(file => {
     const fileData = fs.readFileSync(path.join('./lang', file));
-    CSVValidator(fileData).then(csvData => {
+    return CSVValidator(fileData).then(csvData => {
         csvData.data // Array of objects from file
         throw new Error(csvData.inValidData) // Array of error messages
     })
-  });
+  }));
 }
