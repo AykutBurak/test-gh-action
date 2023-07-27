1ba00e50d7038f985af2b40ecebabfc5780d3ba0
AykutBurak <a.buraksafak@gmail.com>
2023-07-27 13:29:20 +0300

    fix: path for validate

--- dist/index.js
+++ dist/index.js
@@ -4077,8 +4077,8 @@ const exec = __nccwpck_require__(514);
 
 async function run() {
   try {
-    await exec.exec('python3', ['../validate/validate_json.py']);
-    await exec.exec('python3', ['../validate/validate_csv.py']);
+    await exec.exec('python3', ['./validate/validate_json.py']);
+    await exec.exec('python3', ['./validate/validate_csv.py']);
   } catch (error) {
     core.setFailed(error.message);
   }
